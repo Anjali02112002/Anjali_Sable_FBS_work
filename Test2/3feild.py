e9d24040e5dc601de516024d1b5b0ccd05497bbe
Anjali02112002 <anjalisable182@gmail.com>
2025-08-05 15:29:06 +0530

    Update 3feild.py

--- Test2/3feild.py
+++ Test2/3feild.py
@@ -4,6 +4,7 @@ radius = 20
 cost = 35
 
 perimeter = (2*length + breadth + 3.14*radius)
-total_cost = perimeter*cost
+total_cost = 5*(perimeter*cost)
+
 
 print(f'Total cost of fencing the feild is {total_cost}')
